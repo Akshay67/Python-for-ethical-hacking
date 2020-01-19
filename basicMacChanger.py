42773dab916812ab42feb607a4e4deae8bcf7534
Akshay <akshayshivekar06081995@gmail.com>
2020-01-19 11:37:43 -0500

    basic network scanner program

--- basicMacChanger.py
+++ basicMacChanger.py
@@ -1,4 +1,9 @@
 import subprocess
+import optparse
+
+parser = optparse.OptionParser()
+parser.add_option("-i","--interface", dest="interface", help="This is for to add the interface")
+parser.parse_args()
 
 interface = input("Enter Interface -> ")
 new_mac = input("Enter new mac -> ")
